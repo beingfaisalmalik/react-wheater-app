05010cd3f69717806fbf046bac45f4b94a979e49
myusername <myemail>
2020-07-09 12:15:07 -0700

    Updates

---
+++ precache-manifest.41c84052a41ad1cf82f3bd0f2df09b19.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fe1dcbddc366e68c450c23c5284dbb7",
+    "url": "/beingfaisalmalik/react-wheater-app/index.html"
+  },
+  {
+    "revision": "47f39a7e7554f5f04a57",
+    "url": "/beingfaisalmalik/react-wheater-app/static/css/main.e4b4baa0.chunk.css"
+  },
+  {
+    "revision": "4a79708fe2bb8d98f2f2",
+    "url": "/beingfaisalmalik/react-wheater-app/static/js/2.07144284.chunk.js"
+  },
+  {
+    "revision": "85b12af60ddf57ea6860c3b16407fd52",
+    "url": "/beingfaisalmalik/react-wheater-app/static/js/2.07144284.chunk.js.LICENSE"
+  },
+  {
+    "revision": "47f39a7e7554f5f04a57",
+    "url": "/beingfaisalmalik/react-wheater-app/static/js/main.32c2e689.chunk.js"
+  },
+  {
+    "revision": "d845597dacbed073f8a7",
+    "url": "/beingfaisalmalik/react-wheater-app/static/js/runtime-main.2d86a2c3.js"
+  },
+  {
+    "revision": "c97cb84b74a52edcb496295687e87822",
+    "url": "/beingfaisalmalik/react-wheater-app/static/media/cold-bg.c97cb84b.jpg"
+  },
+  {
+    "revision": "635ed87d2cc826fb213057ac40dfaff1",
+    "url": "/beingfaisalmalik/react-wheater-app/static/media/warm-bg.635ed87d.jpg"
+  }
+]);
