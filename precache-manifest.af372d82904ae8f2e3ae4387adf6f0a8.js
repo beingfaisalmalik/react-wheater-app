06d09fc0e95e1b072b6e0633475f87118d861754
myusername <myemail>
2020-07-09 12:24:04 -0700

    Updates

---
+++ precache-manifest.af372d82904ae8f2e3ae4387adf6f0a8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8568cf546cb4fd176b5144f9f390ebd",
+    "url": "/react-wheater-app/index.html"
+  },
+  {
+    "revision": "5898dfcb13fae6286e24",
+    "url": "/react-wheater-app/static/css/main.c9cae790.chunk.css"
+  },
+  {
+    "revision": "4a79708fe2bb8d98f2f2",
+    "url": "/react-wheater-app/static/js/2.07144284.chunk.js"
+  },
+  {
+    "revision": "85b12af60ddf57ea6860c3b16407fd52",
+    "url": "/react-wheater-app/static/js/2.07144284.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5898dfcb13fae6286e24",
+    "url": "/react-wheater-app/static/js/main.1fdac0a7.chunk.js"
+  },
+  {
+    "revision": "a11b76d2b8f86c4fdcdf",
+    "url": "/react-wheater-app/static/js/runtime-main.2945ba7e.js"
+  },
+  {
+    "revision": "c97cb84b74a52edcb496295687e87822",
+    "url": "/react-wheater-app/static/media/cold-bg.c97cb84b.jpg"
+  },
+  {
+    "revision": "635ed87d2cc826fb213057ac40dfaff1",
+    "url": "/react-wheater-app/static/media/warm-bg.635ed87d.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/beingfaisalmalik/react-wheater-app/precache-manifest.41c84052a41ad1cf82f3bd0f2df09b19.js"
+  "/react-wheater-app/precache-manifest.af372d82904ae8f2e3ae4387adf6f0a8.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/beingfaisalmalik/react-wheater-app/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-wheater-app/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
